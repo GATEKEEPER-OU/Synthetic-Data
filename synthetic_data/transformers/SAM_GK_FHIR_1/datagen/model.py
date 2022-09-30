569b5f90ac4602ca748f5cbd4d010a6a2d6adac3
Audrey Ekuban <81741182+AudreyE2@users.noreply.github.com>
2022-09-30 15:16:38 +0100

    Add files via upload

--- synthetic_data/transformers/SAM_GK_FHIR_1/datagen/model.py
+++ synthetic_data/transformers/SAM_GK_FHIR_1/datagen/model.py
@@ -30,7 +30,6 @@ class DataGenModel:
     import tensorflow as tf
     import numpy as np
     import pandas as pd
-
     np.random.seed(1)
 
     def __init__ (self,
@@ -42,18 +41,14 @@ class DataGenModel:
         self.transformer_vers = transformer_vers
         self.model_dir = model_dir
 
-        self.OBSERVATION_SEQUENCE_LENGTH = 72
+        self.OBSERVATION_SEQUENCE_LENGTH = 100
         self.OBSERVATION_START = "[START]"
         self.OBSERVATION_END = "[END]"
 
         # Load the embeddings
-        doc2vec_fname = self.os.path.join(self.model_dir, 'embeddings', 'doc2vec_size256')
+        doc2vec_fname = self.os.path.join(self.model_dir, 'embeddings', 'doc2vec_size512')
         self.doc2vec_model = self.Doc2Vec.load(doc2vec_fname)
-
-        # codings file
-        self.codings_file = self.os.path.join(self.model_dir, 'embeddings', 'codings', 'codings.csv')
        
-
         def load_model(model_name: str, custom_objects=None):
             import warnings
             warnings.filterwarnings("ignore", category=DeprecationWarning) 
@@ -99,10 +94,9 @@ class DataGenModel:
                     break
             
             self.document_tag = self.start_tag
-            # Check start_tag. Maybe someone has been naughty
             if self.start_tag is not None:
                 self.document = self.document_tag.split('_')[0]
-                self.document_vector = self.doc2vec_model.dv[self.document]
+                self.document_vector = self.doc2vec_model.dv[self.document]       
                 self.start_observation = int(self.document_tag.split('_')[1])
                 self.direction = 'forward'
         else:
@@ -116,6 +110,7 @@ class DataGenModel:
                 else:
                     # We have exhausted all the tags. Reverse.
                     self.direction = 'backward'
+                    print('Reversing')
             
             if self.direction == 'backward':
                 current_observation = int(self.document_tag.split('_')[1])
@@ -129,55 +124,65 @@ class DataGenModel:
                     self.document_tag = self.document + "_" + str(previous_observation)
 
     
-    def _check_document_similarity(self, observation):
-        # Compare like for like
-        observation = observation.replace("{", "").replace("}", "").replace(":", "").replace("'", "").replace('display', '').replace("  ", " ")
+    def _check_document_similarity(self, observations):
+        from sklearn.metrics.pairwise import cosine_similarity
+        
+        most_similar = -1
+        similarity = -1.0
 
+        document_tag_vector = self.doc2vec_model.dv[self.document_tag]
 
-    def _get_codings(self):
-        # We need this for validation of generated data
+        for num_obs, observation in enumerate(observations):
+            query_tokens = observation.split()
+            query_vector = self.doc2vec_model.infer_vector(query_tokens)
 
-        codingDF = self.pd.read_csv(self.codings_file)
-        coding = codingDF['coding'].values.tolist()
-        display = codingDF['display'].values.tolist()
-        guide_text = codingDF['guide_text'].values.tolist()
-        return coding, display, guide_text
+            sim = cosine_similarity(self.np.array([document_tag_vector]), self.np.array([query_vector]))
 
+            if sim[0] > 0 and sim[0] > similarity:
+                similarity = sim[0]
+                most_similar = num_obs
+                 
+        return most_similar
 
     # Generate data for one user
     def generate_single_user(self, n_days=1):
-        coding, display, guide_text = self._get_codings()
+        from datetime import timezone, datetime, timedelta
 
         n_secs = n_days * 86400
         self.start_tag = None
 
-        columns = ['doc_tag', 'event']
+        columns = ['normTime', 'observation']
         resultsDF = self.pd.DataFrame(columns = columns)
 
         min_secs  = None
         max_secs = None
         num_secs = 0
-        temperatures = [0.7, 0.8, 0.9, 1.0]
+
         while True:
             self._get_encoder_input()
             if self.start_tag is None or self.document_tag is None:
                 break
-            
             input = self.document_tag.replace("_", " ")
-
             tokenized_input= self.sourceTextProcessor([input])
-            decoded_output = self.OBSERVATION_START
+            
+            decoded_outputs = []
+            for temp in range(1,11):
+                code = None
+                decoded_output = self.OBSERVATION_START
 
-            for i in range(self.OBSERVATION_SEQUENCE_LENGTH):
-                ok = 0
-                for temperature in temperatures:
+                # Generate data for different temperatures
+                temperature = 0.1 * temp
+                code = None
+
+                for i in range(self.OBSERVATION_SEQUENCE_LENGTH):
+
                     tokenized_target = self.targetTextProcessor([decoded_output])[:, :-1]
 
                     predictions = self.transformer.predict([tokenized_input, tokenized_target], verbose=0)
                     preds = predictions[0, i, :]
 
                     preds = self.np.asarray(preds).astype("float64")
-                
+                    
                     self.np.seterr(divide='ignore')
                     preds = self.np.log(preds) / temperature
                     exp_preds = self.np.exp(preds)
@@ -189,57 +194,66 @@ class DataGenModel:
 
                     if i == 0 and (sampled_token != self.document):
                         # Incorrect document
-                        continue
+                        break
                     elif i == 1 and (self.document + " " + sampled_token) != input:
                         # Incorrect document tag
-                        continue
+                        break
                     elif i == 2:
                         if sampled_token.isdigit():
                             normTime = int(sampled_token)
-                            ok = 1
-                            break
                         else:
-                            # Invalid time
-                            continue
+                            break
                     elif i == 3:
-                        if sampled_token in coding:
-                            code = sampled_token
-                            ok = 1
-                            break
-                        else:
-                            # Invalid Code
-                            continue
-                    else:
-                        ok = 1
+                        code = sampled_token
+
+                    if sampled_token == self.OBSERVATION_END:
                         break
-                
-                if ok == 0:
-                    break
+                    decoded_output += " " + sampled_token
 
-                if sampled_token == self.OBSERVATION_END:
-                    break
-                decoded_output += " " + sampled_token
+                if code is None:
+                    continue
 
-            if ok == 0:
+                decoded_output = decoded_output.replace(self.OBSERVATION_START, "").strip()
+
+                observation = " ".join(decoded_output.split()[4:])
+                if code in observation:
+                    decoded_outputs.append(observation)
+                else:
+                    print(code, observation)
+
+            if len(decoded_outputs) == 0:
                 continue
-            decoded_output = decoded_output.replace(self.OBSERVATION_START, "").strip()
-            print(decoded_output)
+            else:
+                doc_index = self._check_document_similarity(decoded_outputs)
+                if doc_index == -1:
+                    continue
 
-            coding_index = coding.index(code)
-            if display[coding_index] in decoded_output and guide_text[coding_index] in decoded_output:
-                if min_secs is None:
+            if min_secs is None:
                     min_secs =  max_secs = normTime
-                elif min_secs > normTime:
+            elif min_secs > normTime:
                     min_secs = normTime
-                elif max_secs < normTime:
+            elif max_secs < normTime:
                     max_secs = normTime
                 
-                decoded_output = " ".join(decoded_output.split()[2:])
-                resultsDF.loc[len(resultsDF.index)] = [self.document_tag, decoded_output]
-                num_secs = max_secs - min_secs
-                if num_secs >= n_secs:
-                    break
-            else:
-                continue
+            resultsDF.loc[len(resultsDF.index)] = [normTime, decoded_outputs[doc_index]]
+            num_secs = max_secs - min_secs
 
-        return resultsDF
+            if num_secs >= n_secs:
+                break
+        
+        #We want to generate past times
+        resultsDF = resultsDF.sort_values(by=['normTime'])
+        timeNow = datetime.now(timezone.utc) - timedelta(seconds=int(max_secs))
+
+        # Columns for ouput dataframe
+        columns = ['obsTime', 'sleepEnd', 'observation']
+        outputDF = self.pd.DataFrame(columns = columns)
+        for row in resultsDF.itertuples():
+            obsTime = (timeNow + timedelta(seconds=int(row.normTime))).strftime("%Y-%b-%d %X")
+            
+            end = None
+            if '<duration>' in row.observation:  
+                end = obsTime
+
+            outputDF.loc[len(outputDF.index)] = [obsTime, end, row.observation]
+        return outputDF
